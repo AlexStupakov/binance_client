7b67e4bb9b2325a5d2e5d8214d8b55d46bbe42da
Oleksandr Stupakov <stepan.alex@gmail.com>
2019-01-17 22:12:43 +0200

    fail

--- spec/binance_spec.rb
+++ spec/binance_spec.rb
@@ -9,7 +9,7 @@ describe 'Binance' do
       stub_request(:get, /#{endpoint}.*/)
         .to_return(status: 200, body: body, headers: {})
 
-      expect(Binance.account).to eq body
+      expect(Binance.account).to eq body + "bla"
     end
   end
 
